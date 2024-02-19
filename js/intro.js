a954b9ec71a4b4d96a43c8ff1dd01a951ff12080
su24-he <suwenh2@uci.edu>
2024-02-18 22:19:31 -0800

    update

--- js/intro.js
+++ js/intro.js
@@ -1,2 +1,17 @@
 //resource link: https://www.w3schools.com/jquery/default.asp
 
+$(document).ready(function(){
+    $("#box").hide();
+
+    $("#toggle-btn").click(function(){
+        $("#box").toggle();
+    });
+
+    $("#box").click(function(){
+        $(this).animate({width:"50%"}, 500);
+    });
+
+    $("#name").click(function(){
+        $("#box").css("background-color", "white");
+    });
+});
